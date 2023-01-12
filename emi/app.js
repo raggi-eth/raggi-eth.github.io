be83628e3a66a347d3e0ce0b978d4a287c8596c6
raggi-eth <110177990+raggi-eth@users.noreply.github.com>
2023-01-12 20:25:10 +0000

    a
    a

--- emi/app.js
+++ emi/app.js
@@ -11,12 +11,14 @@ function convertVideoToSVG(videoUrl) {
     // Create the grid of divs
     for (var i = 0; i < 44; i++) {
       for (var j = 0; j < 44; j++) {
-        var div = document.createElement('div');
+        var div = document.createElement('block');
         div.style.width = '12px';
         div.style.height = '12px';
         div.style.display = 'flex';
         div.style.justifyContent = 'center';
         div.style.alignItems = 'center';
+        div.style.display = 'flex';
+        div.style.scale = '1.2';
         div.style.animationDelay = j * i + 'ms';
         div.style.animationName = 'scale-animation';
         div.style.animationDuration = '3s';
@@ -86,21 +88,23 @@ function convertVideoToSVG(videoUrl) {
 }
 
 // testing 
+
+
 let animationStates = [
   {
-    columns: 22,
-    transitionDuration: 2000,
+    columns: 412,
+    transitionDuration: 1000,
     TriggerTime: 2000,
     next: 1
   },
   {
     columns: 44,
-    transitionDuration: 1000,
+    transitionDuration: 2000,
     TriggerTime: 8000,
     next: 2
   },
   {
-    columns: 88,
+    columns: 44,
     transitionDuration: 3000,
     TriggerTime: 12000,
     next: 3
@@ -112,7 +116,7 @@ let animationStates = [
     next: 4
   },
   {
-    columns: 154,
+    columns: 4094,
     transitionDuration: 3000,
     TriggerTime: 26000,
     next: 5
@@ -154,10 +158,8 @@ let timer;
 let startTime;
 
 function animate() {
-
   let currentTime = Date.now()
   let elapsedTime = currentTime - startTime;
-  console.log(elapsedTime)
   // let seconds = Math.floor(elapsedTime / 1000);
 
   // get the current state
@@ -165,7 +167,7 @@ function animate() {
   if (currentState.next === 0) return;
   // compare the current time with the trigger time of the current state
   if (elapsedTime > state.TriggerTime) {
-    console.log("tirred")
+    console.log("triggered")
     // if the current time is greater than the trigger time, trigger the animation
     triggerAnimation(state);
     // set the current state to the next state
@@ -175,6 +177,9 @@ function animate() {
 }
 
 function triggerAnimation(state,) {
+
+  if (!animateOn) return;
+
   // get the container element
   let container = document.getElementById('divContainer');
   // get the current number of columns --columns: repeat(<this value>, 1fr);
@@ -216,7 +221,7 @@ function triggerAnimation(state,) {
   }
   // start the animation by setting the start time and starting the interval
   startTime = Date.now();
-  timer = setInterval(animate, 1000 / 60);
+  timer = setInterval(animate, 1000 / 24);
 }
 
 
@@ -257,36 +262,165 @@ function convertBitmapToSVG(bitmap) {
 }
 
 
-// creata a button that will start the process
-var button = document.createElement('button');
-button.innerHTML = 'Start';
-// style the button and plase it in the DOM, plase it in the top right corner
-button.style.position = 'absolute';
-button.style.top = '0';
-button.style.right = '0';
-button.style.zIndex = '100';
-document.body.appendChild(button);
-// add an event listener to the button, on click start the process
-button.addEventListener('click', async function () {
-  convertVideoToSVG('https://nftstorage.link/ipfs/bafybeie4jrftpqz2jhpkfuqtnrowrezm4whrpsnzhw4cmwrwdpli7ytgwa');
-});
+
+
+let rowGap;
+let columnGap;
+let scale;
+let columns;
 
 var slider = document.getElementById("columnsSlider");
-
-// Update the grid when the slider value changes
-// make it a log scale from 0 to 4096
 slider.max = 200;
 slider.min = 1;
 slider.value = 100;
 slider.step = 0.5;
-
-
 slider.oninput = function () {
   console.log(this.value);
   if (this.value == 0) {
     this.value = 1;
   }
   document.getElementById("divContainer").style.scale = this.value / 100;
-  
-} 
+  scale = this.value / 100;
+
+}
+var slider2 = document.getElementById("columnsSlider2");
+slider2.max = 40;
+slider2.min = 0;
+slider2.value = 0;
+slider2.step = 0.1;
+slider2.oninput = function () {
+  console.log(this.value);
+  if (this.value == 0) {
+    this.value = 1;
+  }
+  document.getElementById("divContainer").style.rowGap = this.value + "px";
+  rowGap = this.value;
+
+}
+
+var slider3 = document.getElementById("columnsSlider3");
+slider3.max = 1353;
+slider3.min = 1;
+slider3.value = 44;
+slider3.step = 1;
+slider3.oninput = function () {
+  console.log(this.value);
+  if (this.value == 0) {
+    this.value = 1;
+  }
+  document.getElementById("divContainer").style.gridTemplateColumns = 'repeat(' + this.value + ', 1fr)';
+  columns = this.value;
+
+}
+
+var slider4 = document.getElementById("columnsSlider4");
+slider4.max = 40;
+slider4.min = 0;
+slider4.value = 0;
+slider4.step = .1;
+slider4.oninput = function () {
+  console.log(this.value);
+
+  document.getElementById("divContainer").style.columnGap = this.value + "px";
+  columnGap = this.value;
+}
+
+// create a div that 
+var sliderInfoDisplay = document.createElement('div');
+
+sliderInfoDisplay.innerHTML = 'Slider info';
+// style the button and plase it in the DOM, plase it in the top right corner
+sliderInfoDisplay.style.position = 'relative';
+sliderInfoDisplay.style.width = '400px';
+sliderInfoDisplay.style.height = '200px';
+sliderInfoDisplay.style.top = '0';
+sliderInfoDisplay.style.left = '0';
+sliderInfoDisplay.style.zIndex = '100';
+// place it in the sliderContainer
+document.getElementById("sliderContainer").appendChild(sliderInfoDisplay);
+
+let scaleAnimation = 1;
+var scaleAnimationSlider = document.getElementById("scaleAnimationSlider");
+scaleAnimationSlider.max = 4;
+scaleAnimationSlider.min = 0;
+scaleAnimationSlider.value = 1;
+scaleAnimationSlider.step = 0.01;
+scaleAnimationSlider.oninput = function () {
+  console.log(this.value);
+  if (this.value == 0) {
+    this.value = 1;
+  }
+  scaleAnimation = this.value;
+  document.documentElement.style.setProperty('--scaleAnimation', this.value);
+}
+
+// every 100ms update the sliderInfoDisplay
+setInterval(function () {
+  sliderInfoDisplay.innerHTML = '{' + '</br>' + 'transitionDuration: 0,</br> TriggerTime: 0, </br> next: 0,</br>' + 'scale: ' + scale + ',' + '</br>' + ' rowGap: ' + rowGap + ',' + '</br>' + ' columnGap: ' + columnGap + ',' + '</br>' + ' columns: ' + columns + '</br>' +'scaleAnimation: ' + scaleAnimation + '</br>' + '}';
+}, 100);
+
+
+
+
+// a button that is a boolean, if true the animation function will run/ plase it in the top of the sliderContainer. it should be darker when on and lighter when off
+var animateOn = false;
+var animateButton = document.createElement('button');
+animateButton.innerHTML = 'Animate is off';
+animateButton.style.backgroundColor = 'rgb(0, 0, 0)';
+animateButton.style.color = 'rgb(255, 255, 255)';
+// style the button and plase it in the DOM, plase it in the top right corner
+animateButton.style.position = 'absolute';
+// plase it in the top of the #sliderContainer
+animateButton.style.top = '0';
+animateButton.style.left = '0';
+animateButton.style.zIndex = '100';
+animateButton.style.marginTop = '-50px';
+document.getElementById('sliderContainer').appendChild(animateButton);
+// add an event listener to the button, on click start the process
+animateButton.addEventListener('click', function () {
+  debugger;
+  if (!animateOn) {
+    animateOn = true;
+    animateButton.style.backgroundColor = 'rgb(255, 255, 255)';
+    animateButton.style.color = 'rgb(0, 0, 0)';
+    animateButton.innerHTML = 'Animate is on';
+  } else {
+    animateOn = false;
+    animateButton.style.backgroundColor = 'rgb(0, 0, 0)';
+    animateButton.style.color = 'rgb(255, 255, 255)';
+    animateButton.innerHTML = 'Animate is off';
+  }
+}
+);
+
+// creata a button that will start the process
+var startButton = document.createElement('button');
+startButton.innerHTML = 'Start';
+// style the button and plase it in the DOM, plase it in the top of the sliderContainer
+startButton.style.backgroundColor = 'rgb(0, 0, 0)';
+startButton.style.color = 'rgb(255, 255, 255)';
+// style the button and plase it in the DOM, plase it in the top right corner
+startButton.style.position = 'absolute';
+// plase it in the top of the #sliderContainer
+startButton.style.top = '0';
+startButton.style.left = '0';
+startButton.style.zIndex = '100';
+startButton.style.marginTop = '-100px';
+
+document.getElementById('sliderContainer').appendChild(startButton);
+// add an event listener to the button, on click start the process
+startButton.addEventListener('click', async function () {
+  convertVideoToSVG('https://nftstorage.link/ipfs/bafybeie4jrftpqz2jhpkfuqtnrowrezm4whrpsnzhw4cmwrwdpli7ytgwa');
+});
+
+// a slider to change @keyframes scale-animation {
+  // variable in css
+  // :root {
+  //   --scaleAnimation: 0;
+  // }
+
+
+
+
+
 
